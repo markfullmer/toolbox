c2c0d857ac3fde9475c8dcadb8b8c02e299a7068
Mark Fullmer <jfullmer@austin.utexas.edu>
2020-12-30 10:44:40 -0600

    Add composer-updater.sh

--- toolbox.sh
+++ toolbox.sh
@@ -1,55 +1,135 @@
-## Git ##
-gitclone() {
-  # usage: gitclone <project>
-  # or: gitclone <vendor>/<project>
-  # Provide a shortcut default account
-  if [[ "$1" == *\/* ]]; then
-    path=$1
-    project=$(basename $1)
-  else
-    path="markfullmer/"$1
-    project=$1
-  fi
-  # e.g., gitclone toolbox
-  git clone git@github.com:"$path".git
-  # Make sure the clone succeeded before doing anything else.
-  if [ "$?" -eq 0 ]; then
-    cd $project
-    # Convention for remote naming.
-    git remote rename origin github
-    git remote -v
-  fi
-}
+## Terminal shortcuts
+alias la="ls -al"
+alias findhere="find . -name "
+alias aliases='code ~/aliases.sh'
+
+## Git shortcuts
+alias gs="git status --porcelain"
+alias gfgc="git fetch && git checkout"
 drupalclone() {
   # usage: drupalclone token
   git clone git@git.drupal.org:project/"$1".git
   cd $1
+  git config user.email mfullmer@gmail.com
+}
+austinclone() {
+  # usage: austinclone utdk_profile
+  git clone git@github.austin.utexas.edu:eis1-wcs/"$1".git
+  cd $1
   git remote rename origin github
+  PANTHEON_SITE_GIT_URL="$(terminus connection:info $1.dev --field=git_url)"
+  git remote add pantheon "$PANTHEON_SITE_GIT_URL"
 }
+github() {
+  # (MacOS only)
+  # usage: github <repository name>
+  # e.g., github utdk_profile
+  open https://github.austin.utexas.edu/eis1-wcs/"$1"
+}
+## Other useful Git integrations
+# - The official Github CLI: https://cli.github.com/
+# - Github Release Notes Generator: https://github.com/github-tools/github-release-notes
 
-## Pantheon ##
-
+## Pantheon/Drupal integration
+complete-update() {
+  # usage: complete-update txglobal update-1112
+  git fetch github && git checkout $2
+  gh pr merge -s
+  git pull github master
+  git push pantheon master
+  deployToTestF $1
+}
+migrate-sync() {
+  export SOURCE_SITE="utqs-migration-tester"
+  fin db create utexas_migrate && \
+  terminus env:wake $SOURCE_SITE.dev && \
+  terminus drush $SOURCE_SITE.dev cc all && \
+  terminus backup:create $SOURCE_SITE.dev --element=db && \
+  terminus backup:get $SOURCE_SITE.dev  --element=db --to=./db.sql.gz && \
+  gunzip -c ./db.sql.gz > db.sql && \
+  fin db import db.sql --db=utexas_migrate && \
+  rm db.sql db.sql.gz
+}
+pantheon-sync() {
+  terminus drush $1.live cr && \
+  terminus backup:create $1.live --element=db && \
+  terminus backup:get $1.live  --element=db --to=./db.sql.gz && \
+  lando db-import db.sql.gz
+}
 dashboard() {
-  # usage: dashboard <site:name>
-  # e.g., dashboard utqs-staff-council
-  ID=$(terminus site:info "$1" --field=ID)
-  open https://dashboard.pantheon.io/sites/"$ID"#dev/code
+  # usage: dashboard <site-name>
+  # or `dashboard <site-name>.<environment>
+  # e.g., dashboard utqs-staff-council.test
+  IFS=. PARTS=(${1})
+  SITE=${PARTS[0]}
+  ID=$(terminus site:info "$SITE" --field=ID)
+  if [ ! -z "${PARTS[1]}" ];
+  then
+    ENV="${PARTS[1]}"
+  else
+    ENV="dev"
+  fi
+  open https://dashboard.pantheon.io/sites/"$ID"#"$ENV"/code
 }
-deployToTest() {
-  # usage: deployToTest <site:name>
-  # e.g., deployToTest utexas-eureka
+deployTest() {
+  # usage: deployTest <site:name>
+  # e.g., deployTest utexas-eureka
+  # optional deployment message:
+  # deployTest utexas-core "123 - Update fractals"
   USER=$(git config user.name)
-  terminus env:deploy "$1".test --sync-content --note=''"$USER"': deploy to TEST' --cc --updatedb
+  if [ -z "$1" ];
+  then
+    echo 'You must supply the machine name of a Pantheon site (e.g., utexas-core)'
+    return
+  fi
+  if [ ! -z "$2" ];
+  then
+    MESSAGE="$2"
+  else
+    MESSAGE="deploy to TEST"
+  fi
+  terminus env:deploy "$1".test --sync-content --note=''"$USER"': '"$MESSAGE"'' --cc --updatedb
 }
-deployToLive() {
-  # usage: deployToLive <site:name>
-  # e.g., deployToLive utexas-eureka
+deployTestF() {
+  # usage: deployTestF <site:name>
+  # e.g., deployTestF utexas-eureka
+  # optional deployment message:
+  # deployTestF utexas-core "123 - Update fractals"
   USER=$(git config user.name)
-  terminus env:deploy "$1".live --note=''"$USER"': deploy to LIVE' --cc --updatedb
+  if [ -z "$1" ];
+  then
+    echo 'You must supply the machine name of a Pantheon site (e.g., utexas-core)'
+    return
+  fi
+  if [ ! -z "$2" ];
+  then
+    MESSAGE="$2"
+  else
+    MESSAGE="deploy to TEST"
+  fi
+  terminus env:deploy "$1".test --note=''"$USER"': '"$MESSAGE"'' --cc --updatedb
+}
+deployLive() {
+  # usage: deployLive <site:name>
+  # e.g., deployLive utexas-eureka
+  # optional deployment message:
+  # deployLive utexas-core "123 - Update fractals"
+  USER=$(git config user.name)
+  if [ -z "$1" ];
+  then
+    echo 'You must supply the machine name of a Pantheon site (e.g., utexas-core)'
+    return
+  fi
+  if [ ! -z "$2" ];
+  then
+    MESSAGE="$2"
+  else
+    MESSAGE="deploy to LIVE"
+  fi
+  terminus env:deploy "$1".live --note=''"$USER"': '"$MESSAGE"'' --cc --updatedb
 }
 
-# Miscellaneous / Just for fun ##
-
+## Potpourri
 lmgtfy() {
   # usage: lmgtfy how do I add a bash function in my profile
   with_spaces=$@
